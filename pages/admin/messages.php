249d00853362a09b817e991b59c58ec05cb69cc4
adlebr <adele.lebrun@student.junia.com>
2024-04-27 17:27:35 +0200

    page message admin
    message.php modifié
    message.css créé

--- pages/admin/messages.php
+++ pages/admin/messages.php
@@ -11,13 +11,56 @@ if (!$userInfo['admin']) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/util/common.php');
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/util/connection.php');
+    getPageHead('L\'équipe Octime', 'team')
+        ?>
+    <link rel="stylesheet" href="../../css/messages.css">
 </head>
 <body>
+    <?php
+    getPageHeader('contact', $userInfo);
+    ?>
+
     <main>
-        A modifier SVP :)
+        <?php
+        $req = "SELECT messages.subject, messages.content, messages.img_extension, messages.id, accounts.name, accounts.surname, accounts.email
+        FROM messages 
+        JOIN accounts ON messages.user_id = accounts.id";
+        $stmt = $connection->prepare($req);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($result as $res){
+            echo "<br>";
+            echo "<div class='message'>";
+            echo "<span>Nom :</span>" . " " . $res['name'];
+            echo "<br>";
+            echo "<span>Prénom :</span>" . " " . $res['surname'];
+            echo "<br>";
+            echo "<span>Email :</span>" . " " . $res['email'];
+            echo "<br><br>";
+            echo "<span>Sujet :</span>" . " " . $res["subject"];
+            echo "<br><br>";
+            echo "<span>Message :</span>";
+            echo "<br>";
+            echo $res["content"];
+            echo "<br>";
+            if($res["img_extension"] != null){
+                $img = '../../images/messages/'.$res["id"].'.'.$res["img_extension"];
+                echo "<br>";
+                echo "<img src='$img'/>";
+            }
+            echo "</div>";
+            echo "<br><br>";
+        }
+        ?>
     </main>
+
+    <?php
+    getPageTopButton();
+    getPageFooter();
+    ?>
 </body>
 </html>
